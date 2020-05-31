6fd5425b5893c4a80429f8166a1541b169ea9436
IgnacioHerreria <iherreria918@live.com>
2020-05-31 16:53:24 -0500

    Model schema user
    Properties, clean properties on prototype, message valitation

--- models/userModel.js
+++ models/userModel.js
@@ -1,15 +1,32 @@
-var mongoose = require("mongoose");
+"use strict";
 
+var mongoose = require("mongoose");
+var uniqueValidator = require("mongoose-unique-validator");
+let ROLES = {
+  values: ["ADMIN", "USER"],
+  message: "{VALUE} not is a valid Rol",
+};
 var Schema = mongoose.Schema;
 
 var userSchema = new Schema({
-    name: { type: String, required: [true, "Name is required"] },
-    mail: { type: String, unique: true, required: [true, "Mail is required"] },
-    password: { type: String, required: [true, "Password is required"] },
-    image: { type: String },
-    rol: { type: String, required: true, default: "USER_ROL" },
-    token: { type: String },
-    Session: { type: Boolean },
+  mail: { type: String, unique: true, required: [true, "Mail is required"] },
+  name: { type: String, required: [true, "Name is required"] },
+  password: { type: String, required: [true, "Password is required"] },
+  image: { type: String },
+  rol: { type: String, required: true, default: "USER", enum: ROLES },
+  token: { type: String },
+  refreshToken: { type: String },
+  session: { type: Boolean, default: "false" },
+  state: { type: Boolean, default: "true" },
 });
 
+//Delete property of prototype when print object json
+userSchema.methods.toJSON = function () {
+  let user = this;
+  let userObject = user.toObject();
+  delete userObject.password;
+  return userObject;
+};
+
+userSchema.plugin(uniqueValidator, { message: "{PATH} can be unique" });
 module.exports = mongoose.model("user", userSchema);
