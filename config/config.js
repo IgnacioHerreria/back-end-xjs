83ce149c56f3c420ffc75d7794d999c30d402bef
IgnacioHerreria <iherreria918@live.com>
2020-05-31 19:44:28 -0500

    delete line

--- config/config.js
+++ config/config.js
@@ -2,11 +2,10 @@
 //  PORT
 // ============================
 process.env.PORT = process.env.PORT || 3000;
+process.env.NODE_ENV = process.env.NODE_ENV || ENV_DEV;
 
 const ENV_DEV = "DEV";
 const DB_SCHEMA = "fisioterapia";
-
-process.env.NODE_ENV = process.env.NODE_ENV || ENV_DEV;
 
 if (process.env.NODE_ENV === ENV_DEV) {
   process.env.URI_MONGODB = "mongodb://localhost:27017/";
