e7e743c6f87371354c10608f90b5910df48e18c4
IgnacioHerreria <iherreria918@gmail.com>
2020-06-21 21:53:31 -0500

    config
    Set variable seed

--- config/config.js
+++ config/config.js
@@ -1,11 +1,14 @@
 const ENV_DEV = "DEV";
 const DB_SCHEMA = "fisioterapia";
 
-
 process.env.PORT = process.env.PORT || 3000;
 process.env.NODE_ENV = process.env.NODE_ENV || ENV_DEV;
+process.env.SEED_TOKEN = process.env.SEED_TOKEN || "eyJhbGccmd";
 
 if (process.env.NODE_ENV === ENV_DEV) {
-  process.env.URI_MONGODB = "mongodb://localhost:27017/";
+    process.env.URI_MONGODB = "mongodb://localhost:27017/";
 }
 process.env.URI_MONGODB = process.env.URI_MONGODB + DB_SCHEMA;
+
+//Time duration token
+process.env.TIME_TOKEN = 60 * 60 * 24;
--- routes/user.js
+++ routes/user.js
@@ -1,16 +1,18 @@
 var express = require("express");
 var app = express();
 
+const { verificacionToken } = require("../middlewares/auth");
+
 var userC = require("../controllers/userController");
 var multiParty = require("connect-multiparty");
 var md_upload = multiParty({ uploadDir: "./upload/user" });
 
-app.get("/usrs/:total?", userC.GetUsers);
-app.get("/:id", userC.GetUser);
-app.post("/crt", userC.CreateUser);
-app.post("/img", md_upload, userC.UploadImage);
-app.get("/img/:id", userC.getUserImage);
-app.put("/:id", userC.UpdateUser);
-app.delete("/:id", userC.DeleteUser);
+app.get("/usrs/:total?", verificacionToken, userC.GetUsers);
+app.get("/:id", verificacionToken, userC.GetUser);
+app.post("/crt", verificacionToken, userC.CreateUser);
+app.post("/img", verificacionToken, md_upload, userC.UploadImage);
+app.get("/img/:id", verificacionToken, userC.getUserImage);
+app.put("/:id", verificacionToken, userC.UpdateUser);
+app.delete("/:id", verificacionToken, userC.DeleteUser);
 
 module.exports = app;
